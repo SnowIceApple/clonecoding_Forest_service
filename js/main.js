2236524cd6a9b817273981a7f5ce405167f70046
SnowIceApple <154928907+SnowIceApple@users.noreply.github.com>
2024-11-11 01:07:24 +0900

    pc main_notice까지 작업

--- js/main.js
+++ js/main.js
@@ -2,13 +2,19 @@ $(document).ready(function(){
 
     var header = $('#header');
     var headerBot = $('.header_bot_box');
+    var headerBot2 = $('.header_bot');
 
     $('#main_nav .mn_floor1 > li > a').on('mouseover focusin', function(){
         header.addClass('nav_active');
     });
 
-    !$('.header_bot').on('mouseleave focusout', function(){
+    $('.header_bot').on('mouseleave', function(){
         header.removeClass('nav_active');
+    });
+
+    $('.hb_right').on('focusin', function(){
+      header.removeClass('nav_active');
+      $('#main_nav .mn_floor1 li').removeClass('active');
     });
 
 
@@ -19,6 +25,8 @@ $(document).ready(function(){
     else{
       basicHeight = 90;
     }
+
+    var marginHeight = 25;
 
 //     var el = $('#first'),
 //     curHeight = el.height(),
@@ -44,8 +52,12 @@ $(document).ready(function(){
 
         headerBot.stop().animate({
             height: activeHeaderHeight,
+            paddingBottom: 25,
         }, 300);
 
+        headerBot2.stop().animate({
+          paddingBottom: 25,
+        }, 300);
 
         tg.children('.mega_menu').css({
           opacity: 1,
@@ -75,7 +87,14 @@ $(document).ready(function(){
         var tg = $(this);
         headerBot.stop().animate({
           height: basicHeight,
+          paddingBottom: 0,
+
       }, 300); 
+
+      headerBot2.stop().animate({
+        paddingBottom: 0,
+      }, 300);
+
 
       $('.mega_menu').css({
         opacity: 1,
@@ -98,11 +117,22 @@ $(document).ready(function(){
         var tg = $(this);
         headerBot.stop().animate({
           height: basicHeight,
+          paddingBottom: 0,
+
       }, 300); 
+
+      headerBot2.stop().animate({
+        paddingBottom: 0,
+      }, 300);
+
+
       $('.mega_menu').css({
-        opacity: 0,
-        display: 'none',
-    }, 0);
+        opacity: 1,
+    }).stop().animate({
+      opacity: 0,
+    }, 300, function(){
+        $(this).css('display', 'none');
+    });
     });
 
 
@@ -129,6 +159,76 @@ $(document).ready(function(){
         basicHeight = 90;
         headerBot.css('height', basicHeight + 'px');
       }
+    });
+
+    $('.main_notice_slide').slick({
+      infinite : true,
+      autoplay : true,
+      prevArrow: $('.mns_prev'),
+      nextArrow: $('.mns_next'),
+    });
+
+    $('.mns_btn.onOff').on('click', function(){
+      if($(this).hasClass('pause')){
+          $(this).removeClass('pause').addClass('play');
+          $('.main_notice_slide').slick('slickPause');
+          $(this).children('.hidden_text').text('슬라이드 자동재생 시작');
+      }
+      else if($(this).hasClass('play')){
+          $(this).removeClass('play').addClass('pause');
+          $('.main_notice_slide').slick('slickPlay');
+          $(this).children('.hidden_text').text('슬라이드 자동재생 정지');
+      }
+    });
+
+    $('.mnss_1').slick({
+      infinite : true,
+      autoplay : true,
+      speed: 1300,
+      fade: true,
+      prevArrow: $('.mss_prev1'),
+      nextArrow: $('.mss_next1'),
+    });
+
+        $('.mss_onOff1').on('click', function(){
+      if($(this).hasClass('pause')){
+          $(this).removeClass('pause').addClass('play');
+          $('.mnss_1').slick('slickPause');
+          $(this).children('.hidden_text').text('슬라이드 자동재생 시작');
+      }
+      else if($(this).hasClass('play')){
+          $(this).removeClass('play').addClass('pause');
+          $('.mnss_1').slick('slickPlay');
+          $(this).children('.hidden_text').text('슬라이드 자동재생 정지');
+      }
+    });
+
+    $('.mnss_2').slick({
+      infinite : true,
+      autoplay : true,
+      speed: 1300,
+      fade: true,
+      prevArrow: $('.mss_prev2'),
+      nextArrow: $('.mss_next2'),
+    });
+
+    $('.mss_onOff2').on('click', function(){
+      if($(this).hasClass('pause')){
+          $(this).removeClass('pause').addClass('play');
+          $('.mnss_2').slick('slickPause');
+          $(this).children('.hidden_text').text('슬라이드 자동재생 시작');
+      }
+      else if($(this).hasClass('play')){
+          $(this).removeClass('play').addClass('pause');
+          $('.mnss_2').slick('slickPlay');
+          $(this).children('.hidden_text').text('슬라이드 자동재생 정지');
+      }
+    });
+
+    $('.mt_btn').each(function(){
+      $(this).on('click', function(){
+        $(this).closest('li').addClass('active').siblings().removeClass('active');
+      });
     });
 
 });
